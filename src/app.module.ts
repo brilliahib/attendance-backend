6251f1e691cae9a4a9a79d2ad4406148bd3be526
brilliahib <brilliahib21@gmail.com>
2026-04-14 14:46:14 +0700

    feat(attendance): implement attendance module (check-in, check-out, user attendance & admin monitoring)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { EmployeeModule } from './modules/employee/employee.module';
+import { AttendanceModule } from './modules/attendance/attendance.module';
 
 @Module({
-  imports: [AuthModule, EmployeeModule],
+  imports: [AuthModule, EmployeeModule, AttendanceModule],
   controllers: [AppController],
   providers: [AppService],
 })
